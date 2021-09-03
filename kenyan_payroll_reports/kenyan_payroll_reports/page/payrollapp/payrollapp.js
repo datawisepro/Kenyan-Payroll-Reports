cdfbc0fd9658a1f676acbbd85eeff636e480a2bc
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2021-09-03 18:17:21 +0300

    Fixing of errors

--- kenyan_payroll_reports/kenyan_payroll_reports/page/payrollapp/payrollapp.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['payrollapp'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Kenyan Payroll Reports',
-		single_column: true
-	});
-}
